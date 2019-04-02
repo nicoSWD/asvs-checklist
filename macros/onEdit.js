e6d433639337c4d4b050468186c2f20f56fc54ac
Nicolas Oelgart <nicolas.oelgart@atrapalo.com>
2019-04-02 18:11:12 +0200

    Add macros

---
+++ macros/onEdit.js
@@ -0,0 +1,12 @@
+function onEdit(event) {
+    var isApplicableRow = 6;
+    var tableRange = "A2:Z99";
+
+    var sheet = SpreadsheetApp.getActiveSpreadsheet();
+    var editedCell = sheet.getActiveCell();
+
+    if (editedCell.getColumn() == isApplicableRow) {
+        var range = sheet.getRange(tableRange);
+        range.sort({ column : isApplicableRow, ascending: false });
+    }
+}
